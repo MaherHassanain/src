67893483bfb531d9cc59acaa5950e0f9233261c7
MaherHassanain <Maher_Hassanain@hotmail.com>
2019-10-02 16:25:20 -0400

    Add files via upload

---
+++ Plane.h
@@ -0,0 +1,45 @@
+/*
+ * Plane.h
+ *
+ *  Created on: Sep 18, 2019
+ *      Author: Maher
+ */
+
+#ifndef PLANE_H_
+#define PLANE_H_
+
+class Plane {
+private:
+	int plane_id;
+	int speed_x;
+	int speed_y;
+	int speed_z;
+	int x;
+	int y;
+	int z;
+	int entry_time;
+
+
+
+public:
+	Plane(int id, int spx, int spy, int spz, int x1, int y1, int z1, int t);
+	virtual ~Plane();
+	int get_plane_id();
+	int get_plane_speed_x();
+	int get_plane_speed_y();
+	int get_plane_speed_z();
+	int get_plane_x();
+	int get_plane_y();
+	int get_plane_z();
+	int get_plane_entry_time();
+	void set_plane_id(int id);
+	void set_plane_speed_x(int spx);
+	void set_plane_speed_y(int spy);
+	void set_plane_speed_z(int spz);
+	void set_plane_x(int x1);
+	void set_plane_y(int y1);
+	void set_plane_z(int z1);
+	void set_plane_entry_time(int t);
+};
+
+#endif /* PLANE_H_ */
---
+++ Project.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include"Plane.h"
+using namespace std;
+
+Plane* planeArray[20];
+
+int main() {
+
+	int airplane_schedule[160] = {0, -641, 283, 500, 95000, 101589, 10000, 13,
+		1, -223, -630, -526, 71000, 100000, 13000, 16,
+		-1, -180, -446, -186, 41000, 100000, 6000, 31,
+		3, 474, -239, 428, 38000, 0, 14000, 44,
+		-1, -535, 520, 360, 95000, 100000, 1000, 49,
+		-1, -164, -593, -501, 83000, 100000, 12000, 67,
+		6, 512, 614, 520, 86000, -1571, 9000, 87,
+		7, -275, 153, -399, 47000, 100000, 12000, 103,
+		8, -129, 525, -300, 92000, 100000, 1000, 123,
+		9, 437, 574, 339, 32000, 0, 8000, 129,
+		10, 568, 538, 192, 50000, 0, 4000, 133,
+		11, 347, 529, -599, 83000, -1799, 10000, 140,
+		12, -512, -482, 578, 35000, 100000, 4000, 142,
+		13, 619, -280, 194, 74000, 0, 10000, 157,
+		14, -141, 427, -321, 41000, 102251, 11000, 165,
+		15, -199, 242, -205, 56000, 100000, 4000, 172,
+		16, 315, -197, -365, 77000, 0, 1000, 187,
+		17, -138, 455, 602, 23000, 102290, 14000, 199,
+		18, -150, 557, -356, 38000, 100000, 1000, 204,
+		19, 194, 184, 598, 35000, 0, 2000, 221,
+		};
+
+	int counter = 0;
+
+
+
+	for (int i = 0; i < 160; i++) {
+
+		planeArray[counter] = new Plane(airplane_schedule [i] , airplane_schedule [i+1], airplane_schedule [i+2], airplane_schedule [i+3], airplane_schedule [i+4],
+				airplane_schedule [i+5], airplane_schedule [i+6], airplane_schedule [i+7]);
+
+		i = i + 7;
+//		cout << planeArray[counter]->get_plane_id() <<endl;
+		counter++;
+
+	}
+
+	cout <<"Plane1 ID is : "<< planeArray[0]->get_plane_id() <<endl;
+	cout << "Plane2 ID is : "<< planeArray[1]->get_plane_id() <<endl;
+	cout <<"Plane3 Entry Time is : "<< planeArray[2]->get_plane_entry_time() <<endl;
+
+
+	return 0;
+}
