aed62c7a992e8a91a2d6daf7ef7069364e7fdb51
FruitPunchSamuraiG <vorig@live.ca>
2019-11-24 17:51:41 -0500

    string parser

---
+++ MessageParser.h
@@ -0,0 +1,18 @@
+//
+// Created by screwdriver on 2019-11-24.
+//
+
+#ifndef UNTITLED_MESSAGEPARSER_H
+#define UNTITLED_MESSAGEPARSER_H
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class MessageParser {
+public:
+   vector<string> split(string s, string delimiter);
+};
+
+
+#endif //UNTITLED_MESSAGEPARSER_H
